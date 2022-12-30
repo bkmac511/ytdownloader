18afd951fbea7706903c73542631f6a72c1f3a0f
Bartosz Maciejewski <bkmac511@gmail.com>
2022-12-30 17:32:08 +0100

    it works

--- main.py
+++ main.py
@@ -4,6 +4,23 @@ link = input("Podaj link do nagrania z YouTube: ")
 
 yt = YouTube(link)
 
-video = yt.streams.first()
+all_streams = yt.streams
 
-video.download('C:/Users/Bartek/Videos')
+resolutions = set()
+
+for stream in all_streams:
+    resolutions.add(stream.resolution)
+
+print("Dostępne rozdzielczości:")
+for i, resolution in enumerate(resolutions):
+    print(f"{i+1}. {resolution}")
+
+resolution = input("Wybierz rozdzielczość (np. 1920x1080): ")
+
+filtered_streams = all_streams.filter(res=resolution)
+
+if filtered_streams:
+    chosen_stream = filtered_streams[0]
+    chosen_stream.download('C:/Users/Bartek/Videos')
+else:
+    print(f"Nie znaleziono nagrania w rozdzielczości {resolution}. Spróbuj wybrać inną.")
